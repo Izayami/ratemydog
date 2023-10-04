0abc78ff79fd4562b181cfd8a138b080a1932b06
Izayami <Izayami@users.noreply.github.com>
2023-10-04 17:57:33 -0400

    Update Cookies.js

--- Code/Cookies.js
+++ Code/Cookies.js
@@ -1,1 +1,12 @@
-Cookies.set('RateMyDog', 'True')
+Cookies.set('RateMyDog', 'True', {expires:2});
+
+var myCookie = Cookies.get("RateMyDog");
+var title = document.querySelector("h1");
+
+if(myCookie) {
+    title.classList.add("cookie");
+}
+
+if (myCookie == "false"){
+    title.classList.remove("cookie");
+}
